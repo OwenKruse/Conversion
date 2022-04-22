f31f700ceb9c70a0973d836d8c797f01fd72d622
OwenKruse <kruse.owen@vashonsd.org>
2022-04-22 09:30:00 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //convert int to binary
+        int x = 5;
+        System.out.println(Integer.toBinaryString(x));
+    }
+
+
+}
